f8e7120d968821684f86b23271a0dbf8cb7611d3
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-06-08 23:27:29 +0300

    sketch about general idea of what I'm gonna do

---
+++ sketch.rb
@@ -0,0 +1,19 @@
+module CSP
+  # How to deal with state machine creation without macro?
+  # We can write a few small extensions for different Ruby implementations.
+  class THreadParker
+    def initialize(pool)
+      @pool = poll
+    end
+
+    private
+
+    attr_reader :pool
+
+    # We handle context of execution in fiber.
+    # When the time is comming we activate this fiber and give it the time in according thread.
+    def park(go_fiber)
+      pool.prepared_thread << go_fiber
+    end
+  end
+end
