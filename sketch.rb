bb623c87e4bec35ae4903d12168429b3d337f3de
Julian Kulesh <raventid@users.noreply.github.com>
2017-07-29 23:08:09 +0300

    some updates for sketch

--- sketch.rb
+++ sketch.rb
@@ -1,6 +1,8 @@
 module CSP
   # How to deal with state machine creation without macro?
   # We can write a few small extensions for different Ruby implementations.
+  # We are targeting MRI, so we can use only one thread at a time. So clojure's core async on JVM is not our best friend, but
+  # ClojureScript might be best inspiration. But ideally it would be really awesome to use poll on JRuby and one thread on MRI
   class ThreadParker
     def initialize(pool)
       @pool = poll
@@ -12,8 +14,14 @@ module CSP
 
     # We handle context of execution in fiber.
     # When the time is comming we activate this fiber and give it the time in according thread.
-    def park(go_fiber)
-      pool.prepared_thread << go_fiber
+    def park(go_block)
+      pool.prepared_thread << go_block
+    end
+  end
+
+  class GoBlock    
+    def run
+      ThreadParker.park(yield)
     end
   end
 end
