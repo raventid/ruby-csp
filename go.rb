e15c2ab802f7744241107b4c9ed372f47fce2112
Julian Kulesh <raventid@users.noreply.github.com>
2017-08-30 23:48:54 +0300

    well, this is really weird, this is wrong
    This file's code is really completely wrong. It does *everything* the wrong way. We should not use channel here, and we should remove subscriber, that was a very stupid concept.

--- go.rb
+++ go.rb
@@ -1,10 +1,16 @@
 class Go
-  def initialize(subscriber)
+  def initialize(subscriber, channel)
     @subscriber = subscriber
+    @channel = channel
   end
   
   # Subscriber is dynamic, so we cannot use this just as it is.
   def <!!
     write_to @subscriber
+  end
+  
+  # Pushes data to channel
+  def !!>
+    write_to @channel
   end
 end
