f9f1d93083d49ded041f4870565cffc8621549a1
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-09-27 10:20:17 +0300

    one more place to remove spaces

--- lib/ruby/csp/go.rb
+++ lib/ruby/csp/go.rb
@@ -3,12 +3,12 @@ class Go
     @subscriber = subscriber
     @channel = channel
   end
-  
+
   # Subscriber is dynamic, so we cannot use this just as it is.
   def <!!
     write_to @subscriber
   end
-  
+
   # Pushes data to channel
   def !!>
     write_to @channel
